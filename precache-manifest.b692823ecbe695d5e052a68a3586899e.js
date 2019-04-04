b948da53f7647a4381ae6c9454a375fbf87b4651
Diego <dvillaverdeprado@gmail.com>
2019-04-04 18:20:56 +0200

    Updates

---
+++ precache-manifest.b692823ecbe695d5e052a68a3586899e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87f22818ba11742a7f8e",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "87f22818ba11742a7f8e",
+    "url": "/contactmanager/static/js/main.bf72cec1.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "2d07bddd60e24c172f6b",
+    "url": "/contactmanager/static/css/2.1c7d2c39.chunk.css"
+  },
+  {
+    "revision": "2d07bddd60e24c172f6b",
+    "url": "/contactmanager/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "c50cf12ba5410469494ff8eeebf0e9c8",
+    "url": "/contactmanager/index.html"
+  }
+];
